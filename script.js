709a58d1f114c3ae952dd45722b4456a7eb1b33c
CroxxN <saradgaihre19@gmail.com>
2022-11-20 01:55:07 +0545

    init

---
+++ script.js
@@ -0,0 +1,45 @@
+let el = document.querySelector(".text");
+console.log("Working");
+let navtext = document.querySelector(".navtext");
+let thing = "red";
+
+let options = {
+  root: null,
+  rootMargin: "0px",
+  threshold: 1.0
+}
+
+let observer = new IntersectionObserver(callback, options)
+function callback  (entries, _observe) {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+
+  navtext.innerText = "Macbook";
+    thing = "red"
+    console.log("Intersecting");
+    }
+  })
+}
+observer.observe(el);
+
+let mode = window.matchMedia("(prefers-color-scheme: dark)").matches;
+console.log(mode);
+if (!mode) {
+  document.body.style.backgroundColor = "white";
+  document.body.style.color = "black";
+} else {
+  document.body.style.backgroundColor = "black";
+  document.body.style.color = "white";
+}
+let darkmode = document.querySelector(".button");
+darkmode.addEventListener("click", () => {
+  if (!mode) {
+    document.body.style.backgroundColor = "black";
+    document.body.style.color = "white";
+    mode = true;
+  } else {
+    document.body.style.backgroundColor = "white";
+    document.body.style.color = "black";
+    mode = false;
+  }
+})
